81ed66e6fbaff8ed86649942aba0b2fe2b405ec4
Liandon <bilinskiy-01@mail.ru>
2022-12-18 12:55:37 +0600

    1

--- task17/Program.cs
+++ task17/Program.cs
@@ -11,6 +11,17 @@ void CheckKoord(int x, int y)
     else Console.WriteLine ("Точка находится на координатной оси");
 }
 
+string CheckKoord2(int x, int y)
+{
+    string res;
+    if (x>0 && y>0) res = "Эта четверть №1";
+    else if (x<0 && y>0) res = "Эта четверть №2";
+    else if (x<0 && y<0) res = "Эта четверть №3";
+    else if (x>0 && y<0) res = "Эта четверть №4";
+    else res = "Точка находится на координатной оси";
+    return(res);
+}
+
 // Тело программы
 try
 {
@@ -19,6 +30,7 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Y");
 int y = Convert.ToInt32(Console.ReadLine());
 CheckKoord(x,y); 
+Console.WriteLine(CheckKoord2(x,y)); 
 }
 catch 
 {
