8cfbee00130bcce627cc0f8b7e6d2da60358def1
Liandon <bilinskiy-01@mail.ru>
2022-12-18 13:41:01 +0600

    2

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+Console.WriteLine("Добрый день!");
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<0) n=-n;
+Console.WriteLine("Таблица кубов до числа " + n + ":");
